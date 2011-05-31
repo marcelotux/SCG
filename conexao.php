f928f5fcd2086a46cb2f667074b67508e7a41d89
marcelotux <Marcelo@.(none)>
2011-05-31 17:07:09 -0300

    first commit

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+
+$conexao = mysql_connect("localhost","sca","sca") or die (mysql_error());
+
+mysql_select_db("db_sca") or die (mysql_error());
+
+
+?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Login</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url(img/bg_linha.gif);
+	background-repeat: repeat-x;
+}
+.style1 {
+	color: #FFFFFF;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 10px;
+}
+-->
+</style></head>
+
+<body>
+<table width="1030" height="326" border="0" align="center">
+  <tr>
+    <td valign="middle">
+      <p align="center">&nbsp;</p>
+      <p align="center">&nbsp;</p>
+      <p align="center">&nbsp;</p>
+      <p align="center">&nbsp;</p>
+      <p align="center">&nbsp;</p>
+        <div align="center">
+          <table width="340" height="211" border="0">
+            <tr>
+              <td height="198" valign="top" background="img/bg_login.gif"><table width="324" border="0">
+                <tr>
+                  <td width="107"><br /></td>
+                  <td colspan="2">&nbsp;</td>
+                </tr>
+                <tr>
+                  <td><div align="right"><img src="img/usuario.png" width="89" height="21" /></div></td>
+                  <td colspan="2"><form id="form1" name="form1" method="post" action="">
+                    <label>
+                      <input type="text" name="textfield" id="textfield" />
+                      </label>
+                    </form>                  </td>
+                </tr>
+                <tr>
+                  <td height="48"><div align="right"><img src="img/senha.png" width="73" height="21" /></div></td>
+                  <td colspan="2"><input type="password" name="textfield2" id="textfield2" /></td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                  <td width="72"><div align="right"><img src="img/botao_limpar.gif" width="71" height="33" /></div></td>
+                  <td width="131"><img src="img/botao_entrar.gif" width="71" height="33" /></td>
+                </tr>
+                <tr>
+                  <td colspan="2"><div align="right" class="style1"></div></td>
+                  <td><span class="style1">Esqueci minha senha<br />
+                  N&atilde;o sou cadastrado</span></td>
+                </tr>
+                <tr>
+                  <td colspan="3"><div align="right"></div></td>
+                </tr>
+              </table></td>
+            </tr>
+          </table>
+        </div>
+      <p align="center">&nbsp;</p>
+      <p align="center">&nbsp;</p>
+    <p align="center">&nbsp;</p>      <p align="center">&nbsp;</p></td>
+  </tr>
+</table>
+</body>
+</html>
